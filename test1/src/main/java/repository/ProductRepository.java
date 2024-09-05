b441afe77c7d17fee9b54029ee5e6a14acf038fd
HyeMin <whgpals641@naver.com>
2024-09-05 17:46:15 +0900

    modify errror

--- test1/src/main/java/repository/ProductRepository.java
+++ test1/src/main/java/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package repository;
 import com.zaxxer.hikari.HikariDataSource;
 import config.HikariCP;
 import domain.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,20 +18,6 @@ public class ProductRepository {
 
     private static HikariDataSource dataSource = HikariCP.createDataSource();
 
-<<<<<<< HEAD
-    public List<Product> selectproduct(){
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try{
-            LOG.info("DB connection success");
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement("select * from product");
-            ResultSet rs = pstmt.executeQuery();
-
-            List<Product> list = new ArrayList<>();
-            while(rs.next()){
-=======
     public List<Product> selectproduct() {
         try (Connection connection = dataSource.getConnection();
         ) {
@@ -37,31 +25,15 @@ public class ProductRepository {
             ResultSet rs = pstmt.executeQuery();
             List<Product> list = new ArrayList<>();
             while (rs.next()) {
->>>>>>> main
                 Product product = new Product();
                 product.setP_id(rs.getInt("p_id"));
                 product.setP_brand(rs.getString("p_brand"));
                 product.setP_kind(rs.getString("p_kind"));
                 product.setP_name(rs.getString("p_name"));
-<<<<<<< HEAD
-                product.setP_conutry(rs.getString("p_conutry"));
-                product.setP_capacity(rs.getString("p_capacity"));
-                product.setP_etc(rs.getString("p_etc"));
-
                 list.add(product);
             } return list;
         }catch (Exception e){
-            e.parameta();
-=======
-                product.setP_conutry(rs.getString("p_country"));
-                product.setP_capacity(rs.getString("pp_capacity"));
-                product.setP_etc(rs.getString("pp_etc"));
-                list.add(product);
-            }
-            return list;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
->>>>>>> main
+            e.printStackTrace();
         }
     }
 }
